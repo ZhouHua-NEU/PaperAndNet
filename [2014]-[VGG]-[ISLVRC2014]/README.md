d8dbf9addcb0af8495f58820585c1fd6bfb930ec
ZHOU HUA <1926718072@qq.com>
2023-03-14 20:49:06 +0800

    Add files via upload

---
+++ [2014]-[VGG]-[ISLVRC2014]/README.md
@@ -0,0 +1,24 @@
+﻿# 网络基本信息-Basic network information
+| 网络名称-Model Name           | VGGnet                                                            |
+|-------------------------------|-------------------------------------------------------------------|s
+| 论文名称-Paper Name           | Very Deep Convolutional Network For Large-Scale Image Recognition |
+| 发表年份-Publish Year         | 2014                                                              |
+| 创新结构-Innovative structure | -------                                                           |
+| 数据集-Datasets               | ISLVRC2014                                                        |
+| 比赛-Competion                | ImageNet                                                          |
+| 改进-Imporve                  | 更小卷积;更深网络-Smaller convolutions; Deeper networks           |
+
+
+# 创新点-Innovation Points
+- **更小的卷积核** 作者使用更小的卷积核代替大的卷积核，网络变得更深，参数量很少，网络感受野变大，模型效果提升。
+
+- **Smaller convolutional kernels** The authors use smaller convolutional kernels instead of large convolutional kernels, the network becomes deeper, the number of parameters is small, the network feeling field becomes larger, and the model effect is improved.
+
+
+# 启示和思考-What we should learn and my think about paper
+- **更小卷积核意味着看的更仔细** 作者使用更小的卷积核，这与ZFnet的中分析的是一样的，更小的卷积核能够提取更丰富的信息，从而为更高级的特征服务。从而更好的进行分类。
+
+
+- **Smaller convolutional kernels mean looking more closely** The authors use smaller convolutional nuclei, which is the same as ZFnet's analysis, smaller convolutional kernels are able to extract richer information and thus serve more advanced features. This allows for better classification.
+
+
---
+++ [2014]-[VGG]-[ISLVRC2014]/code/model/model.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch
+
+
+class VGG(nn.Module):
+    def __init__(self, features, class_num=5):
+        super(VGG, self).__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Linear(4096, class_num)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x,1)
+        x = self.classifier(x)
+        return x
+
+
+def make_features(cfg: list):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg(numclass,model_name="vgg16"):
+    try:
+        cfg = cfgs[model_name]
+    except:
+        print("Warning: model number {} not in cfgs dict!".format(model_name))
+        exit(-1)
+    model = VGG(make_features(cfg),numclass)
+    return model
